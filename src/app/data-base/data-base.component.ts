5b8230b2e0a20b5e4c67cf8813328582ab057df7
Artem <artem.girda@gmail.com>
2021-06-29 11:21:17 +0300

    update layout. added data-base component

--- src/app/data-base/data-base.component.ts
+++ src/app/data-base/data-base.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RestService} from "../shared/services/rest.service";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-data-base',
@@ -7,38 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBaseComponent implements OnInit {
 
-  regions = [
-    {name: 'Обрати всі', id: 1},
-    {name: 'Автономна Республіка Крим', id: 2},
-    {name: 'Вінницька', id: 3},
-    {name: 'Волинська', id: 4},
-    {name: 'Дніпропетровська', id: 5},
-    {name: 'Донецька', id: 6},
-    {name: 'Житомирська', id: 7},
-    {name: 'Закарпатська', id: 8},
-    {name: 'Запорізька', id: 9},
-    {name: 'Івано-Франківська', id: 10},
-    {name: 'Київська', id: 11},
-    {name: 'Кіровоградська', id: 12},
-    {name: 'Луганська', id: 13},
-    {name: 'Львівська', id: 14},
-    {name: 'Миколіївська', id: 15},
-    {name: 'Рівненська', id: 16},
-    {name: 'Сумська', id: 17},
-    {name: 'Тернопільська', id: 18},
-    {name: 'Харківська', id: 19},
-    {name: 'Херсонська', id: 20},
-    {name: 'Хмельницька', id: 21},
-    {name: 'Черкаська', id: 22},
-    {name: 'Чернівецька', id: 23},
-    {name: 'Чернігівська', id: 24},
-    {name: 'м. Київ', id: 25},
-    {name: 'м. Севастополь', id: 26},
-  ]
+  regions$: Observable<any>;
+  subjects$: Observable<any>;
+  periods$: Observable<any>;
 
-  constructor() { }
+  constructor(private rest: RestService) { }
 
   ngOnInit() {
+
+    this.regions$ =this.rest.get(`${environment.apiUrl}/regions`);
+    this.subjects$ =this.rest.get(`${environment.apiUrl}/subjects`);
+    this.periods$ =this.rest.get(`${environment.apiUrl}/periods`);
   }
 
 }
